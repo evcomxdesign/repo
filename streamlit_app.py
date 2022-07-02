326a4292fa62729c46cf30124225481e2512de6a
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-02 17:07:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Object notation
-st.sidebar.[element_name]
+import streamlit as st
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 st.title('Uber pickups in NYC')
 
