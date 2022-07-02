a4a779f209f272c569acc19be65b2a47b6672307
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-02 11:45:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,4 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-
-streamlit run uber_pickups.py
-
 $ streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
