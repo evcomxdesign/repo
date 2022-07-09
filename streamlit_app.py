27c4149af7a2f3b0f52694da71f6a23fe408bbd2
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-09 12:11:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import streamlit as st
+pip install streamlit-aggrid
+from st_aggrid import AgGrid
+AgGrid(my_dataframe)
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
@@ -23,45 +25,3 @@ import time
 
 my_bar = st.progress(0)
 
-for percent_complete in range(100):
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1)
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
-
-@st.cache
-def load_data(nrows):
-         data_load_state.text("Done! (using st.cache)")
-
-st.subheader('Raw data')
-st.write(data)
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
-
-st.subheader('Map of all pickups')
-st.map(data)
-st.subheader('Map of all pickups')
-st.map(data)
-hour_to_filter = 17
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
