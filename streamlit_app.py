05285abb4858b1c0fb38e3b33bfa6d7c4ee7c7d2
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-09 12:36:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import altair as alt
+
+#API reference
+##Write and magic
+write('Hello, *World!* :sunglasses:')
+st.write(1234)
+st.write(pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40],
+ }))
