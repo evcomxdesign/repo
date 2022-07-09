c961fb7e2a0ff11cff17d078462c270a294013a3
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-09 12:15:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-pip install streamlit-aggrid
-from st_aggrid import AgGrid
-AgGrid(my_dataframe)
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://docs.streamlit.io/en/latest")
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
