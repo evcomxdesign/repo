a2e1badf422c288ed64645b10e2b1710643e6fbc
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-10 17:18:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,6 @@ if st.button('Say hello'):
      st.write('Why hello there')
  else:
      st.write('Goodbye')
-     
-     option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
 
