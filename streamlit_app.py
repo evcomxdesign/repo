3dae31ec734fc5e9326e5394f501196e4e2eb40d
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-10 17:34:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,12 @@ chart_data = pd.DataFrame(
      columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
