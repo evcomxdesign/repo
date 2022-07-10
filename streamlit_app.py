c5d8b58c2d2471d08d71277625023a44f1cfc036
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-10 17:19:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(pd.DataFrame({
 
 if st.button('Say hello'):
      st.write('Why hello there')
- else st.write('Goodbye')
+ else: st.write('Goodbye')
 
 st.write('You selected:', option)
 
