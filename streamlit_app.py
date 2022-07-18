f1b81b15b61b07036b68be5d2bd182fb52aef97e
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-18 20:50:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,3 +80,10 @@ with st.sidebar:
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
 
+import time
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.1)
+     my_bar.progress(percent_complete + 1)
