7e895eebdd7c0d5a40a9de2552bdb47476ac3b00
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-18 20:19:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ st.latex(r'''
 
 st.text('This is some text.')
 
-col1, col2, col3 = st.columns(3, gap="large")
+col1, col2, col3 = st.columns(spec, 3, gap="large")
 
 with col1:
     st.header("A cat")
