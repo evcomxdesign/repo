3d5971d3f2b9bb3007452cfe74e0908a835b9dc8
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-18 20:13:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,19 @@ st.latex(r'''
 
 st.text('This is some text.')
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+     
+     
+
