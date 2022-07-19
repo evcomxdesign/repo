987c12997ff2fafcf7369d0f2e1f9f658672f8fa
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-18 21:46:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,3 +70,25 @@ dataframe = pd.DataFrame({
      'second column': [10, 20, 30, 40],
  })
 st.experimental_show(dataframe)
+
+import streamlit as st
+
+def get_user_name():
+    return 'John'
+
+with st.echo():
+    # Everything inside this block will be both printed to the screen
+    # and executed.
+
+    def get_punctuation():
+        return '!!!'
+
+    greeting = "Hi there, "
+    value = get_user_name()
+    punctuation = get_punctuation()
+
+    st.write(greeting, value, punctuation)
+
+# And now we're back to _not_ printing to the screen
+foo = 'bar'
+st.write('Done!')
