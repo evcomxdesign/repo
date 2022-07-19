f6fb523f9587f0d6379b2de071a3c9cc237fad65
evcomxdesign <108580493+evcomxdesign@users.noreply.github.com>
2022-07-18 21:43:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,3 +109,9 @@ if not name:
   st.warning('Please input a name.')
   st.stop()
 st.success('Thank you for inputting a name.')
+
+name = st.text_input('Name')
+if not name:
+  st.warning('Please input a name.')
+  st.stop()
+st.success('Thank you for inputting a name.')
